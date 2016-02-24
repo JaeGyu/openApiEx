0d07f4fd3bc5ca36c1bd6aff29f38dfb571c0ed5
jpmlyr <jpmlyr@gmail.com>
2016-02-24 23:12:05 +0900

    .

--- openApiEx/src/main/java/com/jaegyu/OpenApiExApplication.java
+++ openApiEx/src/main/java/com/jaegyu/OpenApiExApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenApiExApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring boot!");
 		SpringApplication.run(OpenApiExApplication.class, args);
 	}
 }
